d895412ef19658ab3ba7bab6204eef81808a3851
abramovichaj1992 <abramovichaj1992@gmail.com>
2018-12-02 22:27:55 +0300

    test mvc

--- SkiHouse.Web/Startup.cs
+++ SkiHouse.Web/Startup.cs
@@ -15,6 +15,7 @@ namespace SkiHouse.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,7 +25,10 @@ namespace SkiHouse.Web
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseMvc(r=>
+            {
+                r.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
